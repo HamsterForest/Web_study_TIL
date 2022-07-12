4e9bd40dff023cfe993fd9ab20dd5ce7606dfe4c
HamsterForest <republic7788@gmail.com>
2022-07-12 17:37:03 +0900

    nodejs_url입력값사용

---
+++ nodejs연습/main.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData=url.parse(_url,true).query;//쿼리스트링에 따라서 다른 정보를 출력하는 웹 구현
+    
+    
+    if(_url == '/'){
+      _url = '/index.html';
+    }
+    if(_url == '/favicon.ico'){
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+    response.writeHead(200);
+    response.end(queryData.id);
+ 
+});
+
+app.listen(3000);
